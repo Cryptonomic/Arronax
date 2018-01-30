5bfb1bfea67cd5aaa86bd1a431999e996ddfae7a
Vishakh <vishakh@users.noreply.github.com>
2018-01-30 16:45:50 -0500

    Added prototype wrapper library for Conseil.

---
+++ src/Conseil.tsx
@@ -0,0 +1,20 @@
+
+
+function queryNautilus(network: string, command: string, payload: string, callback: (s: any) => any) {
+    let url: string = 'http://localhost:1337/tezos/{$network}/${command}'
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(JSON.parse(xmlHttp.responseText));
+    }
+    xmlHttp.open("GET", url, true);
+    xmlHttp.send(payload);
+}
+
+export function getBlockHead(network: string, callback: (s: any) => any) {
+    queryNautilus(network, 'blocks/head', "", callback)
+}
+
+export function getBlock(network: string, hash: string, callback: (s: any) => any) {
+    queryNautilus(network, 'blocks/${hash}', "", callback)
+}
