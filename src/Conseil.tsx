0ca326c548b5c4771bd0cdd515cf13ffe17fe12d
Vishakh <vishakh@users.noreply.github.com>
2018-03-01 01:55:59 -0500

    Using querystring library to encode Conseil filters.

--- src/Conseil.tsx
+++ src/Conseil.tsx
@@ -1,8 +1,19 @@
+import * as QS from 'querystring';
+
 export interface ConseilFilter {
     limit: number;
+    blockIDs: string[];
+    levels: number[];
+    netIDs: string[];
+    protocols: string[];
+    operationIDs: string[];
+    operationSources: string[];
+    accountIDs: string[];
+    accountManagers: string[];
+    accountDelegates: string[];
 }
 
-function queryNautilus(network: string, command: string, payload: string, callback: (s: string) => void) {
+function submitDataToNautilus(network: string, command: string, payload: string, callback: (s: string) => void) {
     let url: string = 'http://localhost:1337/tezos/{$network}/${command}';
     const xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function() {
@@ -14,10 +25,22 @@ function queryNautilus(network: string, command: string, payload: string, callba
     xmlHttp.send(payload);
 }
 
-export function getBlockHead(network: string, callback: (s: string) => void) {
-    queryNautilus(network, 'blocks/head', '', callback);
+function getDataFromNautilus(network: string, command: string, filter: ConseilFilter, callback: (s: string) => void) {
+    let params: string = QS.stringify(filter)
+    let url: string = 'http://localhost:1337/tezos/{$network}/${command}?{$params}';
+    const xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+            callback(xmlHttp.responseText);
+        }
+    };
+    xmlHttp.open('GET', url, true);
 }
 
-export function getBlock(network: string, hash: string, callback: (s: string) => void) {
-    queryNautilus(network, 'blocks/${hash}', '', callback);
+export function getBlockHead(network: string, callback: (s: string) => void) {
+    submitDataToNautilus(network, 'blocks/head', '', callback);
+}
+
+export function getBlock(network: string, filter: ConseilFilter, callback: (s: string) => void) {
+    submitDataToNautilus(network, 'blocks/${hash}', '', callback);
 }
