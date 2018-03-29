02cb8aa7e7ecc4d3b8fd31fd995c64bd29d9d3e3
Vishakh <vishakh@users.noreply.github.com>
2018-03-28 23:41:54 -0400

    Finished refactor of FilterPanel component to separate local and global state. Removed redux-forms dependency.

--- src/components/Arronax.tsx
+++ src/components/Arronax.tsx
@@ -2,127 +2,159 @@ import * as React from 'react';
 import * as Conseil from '../Conseil';
 import { ConseilFilter } from '../Conseil';
 import { ArronaxState, DataView } from '../types/types';
-import { Field, reduxForm } from 'redux-form';
-import { Component, SyntheticEvent } from 'react';
 
 export interface FilterPanelProps {
     filters: ConseilFilter;
     setFilter:  (filters: ConseilFilter) => void;
 }
 
-function gatherFilters(setFilter:  (filters: ConseilFilter) => void) {
-    setFilter( {
-        blockIDs: (document.getElementById('filter.blockIDs') as HTMLInputElement).value.split(','),
-        levels: (document.getElementById('filter.levels') as HTMLInputElement).value.split(',').map(Number),
-        netIDs: (document.getElementById('filter.netIDs') as HTMLInputElement).value.split(','),
-        protocols: (document.getElementById('filter.protocols') as HTMLInputElement).value.split(','),
-        operationIDs: (document.getElementById('filter.operationIDs') as HTMLInputElement).value.split(','),
-        operationSources: (document.getElementById('filter.operationSources') as HTMLInputElement).value.split(','),
-        accountIDs: (document.getElementById('filter.accountIDs') as HTMLInputElement).value.split(','),
-        accountManagers: (document.getElementById('filter.accountManagers') as HTMLInputElement).value.split(','),
-        accountDelegates: (document.getElementById('filter.accountDelegates') as HTMLInputElement).value.split(','),
-        limit: Number((document.getElementById('filter.limit') as HTMLInputElement).value),
-    });
-}
-function poopypoop(e: React.ChangeEvent<HTMLInputElement>, props: FilterPanelProps) {
-    e.persist();
-    document.getElementById('poop').innerText = e.target.id + e.target.value + props.filters.limit;
-}
-
-const FilterPanel = (props: FilterPanelProps) => {
-    return (
-        <div id="filtergroup">
-            Blocks IDs: <br/>
-            <input type="text" id="filter.blocksIDs" placeholder={props.filters.blockIDs.toString()} /> <br/>
-            Block Levels: <br/>
-            <input type="text" id="filter.levels" placeholder={props.filters.levels.toString()} /> <br/>
-            Net IDs: <br/>
-            <input type="text" id="filter.netIDs" placeholder={props.filters.netIDs.toString()} /> <br/>
-            Protocols: <br/>
-            <input type="text" id="filter.protocols" placeholder={props.filters.protocols.toString()} /> <br/>
-            Operation IDs: <br/>
-            <input type="text" id="filter.operationIDs" placeholder={props.filters.operationIDs.toString()} /> <br/>
-            Operation Sources: <br/>
-            <input type="text" id="filter.blocks" placeholder={props.filters.operationSources.toString()} /> <br/>
-            Account IDs: <br/>
-            <input type="text" id="filter.accounts" placeholder={props.filters.accountIDs.toString()} /> <br/>
-            Account Managers: <br/>
-            <input type="text" id="filter.blocks" placeholder={props.filters.accountManagers.toString()} /> <br/>
-            Account Delegates: <br/>
-            <input
-                type="text"
-                id="filter.blocks"
-                // placeholder={props.filters.accountDelegates.toString()}
-                value="hello"
-                onChange={e => poopypoop(e, props)}
-            /> <br/>
-            Limit: <br/>
-            <input type="text" id="filter.blocks" placeholder={props.filters.limit.toString()} /> <br/>
-            <button onClick={() => gatherFilters(props.setFilter)}>Refresh</button>
-        </div>
-    );
-};
-
-class FilterPanel2 extends React.Component<FilterPanelProps, ConseilFilter> {
+class FilterPanel extends React.Component<FilterPanelProps, ConseilFilter> {
 
     constructor(props: FilterPanelProps) {
         super(props);
         this.state = props.filters;
+        this.handleBlockIDs = this.handleBlockIDs.bind(this);
+        this.handleLevels = this.handleLevels.bind(this);
+        this.handleNetIDs = this.handleNetIDs.bind(this);
+        this.handleProtocols = this.handleProtocols.bind(this);
+        this.handleOperationIDs = this.handleOperationIDs.bind(this);
+        this.handleAccountIDs = this.handleAccountIDs.bind(this);
+        this.handleAccountManagers = this.handleAccountManagers.bind(this);
+        this.handleAccountDelegates = this.handleAccountDelegates.bind(this);
+        this.handleLimit = this.handleLimit.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleChange(event: React.ChangeEvent<HTMLInputElement>) {
-        const foo = event.target.name;
-        if (foo === 'limit') {
-            this.setState({'limit': Number(event.target.value)});
-        } else if (foo === 'levels') {
-            this.setState({'levels': event.target.value.split(',').map(Number)});
-        } else {
-            this.setState({[event.target.name]: [event.target.value]});
-        }
+    handleBlockIDs(event: React.ChangeEvent<HTMLInputElement>) {
+        this.setState({'blockIDs': event.target.value.split(',')});
+    }
 
+    handleLevels(event: React.ChangeEvent<HTMLInputElement>) {
+        this.setState({'levels': event.target.value.split(',').map(Number)});
+    }
+
+    handleNetIDs(event: React.ChangeEvent<HTMLInputElement>) {
+        this.setState({'netIDs': event.target.value.split(',')});
+    }
+
+    handleProtocols(event: React.ChangeEvent<HTMLInputElement>) {
+        this.setState({'protocols': event.target.value.split(',')});
+    }
+
+    handleOperationIDs(event: React.ChangeEvent<HTMLInputElement>) {
+        this.setState({'operationIDs': event.target.value.split(',')});
+    }
+
+    handleOperationSources(event: React.ChangeEvent<HTMLInputElement>) {
+        this.setState({'operationSources': event.target.value.split(',')});
+    }
+
+    handleAccountIDs(event: React.ChangeEvent<HTMLInputElement>) {
+        this.setState({'accountIDs': event.target.value.split(',')});
+    }
+
+    handleAccountManagers(event: React.ChangeEvent<HTMLInputElement>) {
+        this.setState({'accountManagers': event.target.value.split(',')});
+    }
+
+    handleAccountDelegates(event: React.ChangeEvent<HTMLInputElement>) {
+        this.setState({'accountDelegates': event.target.value.split(',')});
+    }
+
+    handleLimit(event: React.ChangeEvent<HTMLInputElement>) {
+        this.setState({'limit': Number(event.target.value)});
+    }
+
+    handleSubmit(event: React.MouseEvent<HTMLButtonElement>) {
+        this.props.setFilter(this.state);
     }
 
     public render() {
         return (
             <div id="filtergroup">
                 Blocks IDs: <br/>
-                <input type="text" id="blocksIDs" value={this.state.blockIDs.toString()} /> <br/>
+                <input
+                    type="text"
+                    id="blocksIDs"
+                    value={this.state.blockIDs.toString()}
+                    onChange={this.handleBlockIDs}
+                /> <br/>
                 Block Levels: <br/>
-                <input type="text" id="levels" value={this.state.levels.toString()} /> <br/>
+                <input
+                    type="text"
+                    id="levels"
+                    value={this.state.levels.toString()}
+                    onChange={this.handleLevels}
+                /> <br/>
                 Net IDs: <br/>
-                <input type="text" id="netIDs" value={this.state.netIDs.toString()} /> <br/>
+                <input
+                    type="text"
+                    id="netIDs"
+                    value={this.state.netIDs.toString()}
+                    onChange={this.handleNetIDs}
+                /> <br/>
                 Protocols: <br/>
-                <input type="text" id="protocols" value={this.state.protocols.toString()} /> <br/>
+                <input
+                    type="text"
+                    id="protocols"
+                    value={this.state.protocols.toString()}
+                    onChange={this.handleProtocols}
+                /> <br/>
                 Operation IDs: <br/>
-                <input type="text" id="operationIDs" value={this.state.operationIDs.toString()} /> <br/>
+                <input
+                    type="text"
+                    id="operationIDs"
+                    value={this.state.operationIDs.toString()}
+                    onChange={this.handleOperationIDs}
+                /> <br/>
                 Operation Sources: <br/>
-                <input type="text" id="blocks" value={this.state.operationSources.toString()} /> <br/>
+                <input
+                    type="text"
+                    id="operationSources"
+                    value={this.state.operationSources.toString()}
+                    onChange={this.handleOperationSources}
+                /> <br/>
                 Account IDs: <br/>
-                <input type="text" id="accounts" value={this.state.accountIDs.toString()} /> <br/>
+                <input
+                    type="text"
+                    id="accounts"
+                    value={this.state.accountIDs.toString()}
+                    onChange={this.handleAccountIDs}
+                /> <br/>
                 Account Managers: <br/>
-                <input type="text" id="blocks" value={this.state.accountManagers.toString()} /> <br/>
-                Account Delegates: <br/>
                 <input
                     type="text"
                     id="blocks"
-                    value="hello"
-                    onChange={this.handleChange}
+                    value={this.state.accountManagers.toString()}
+                    onChange={this.handleAccountManagers}
+                /> <br/>
+                Account Delegates: <br/>
+                <input
+                    type="text"
+                    id="accountDelegates"
+                    value={this.state.accountDelegates.toString()}
+                    onChange={this.handleAccountDelegates}
                 /> <br/>
                 Limit: <br/>
-                <input type="text" id="blocks" value={this.state.limit.toString()} /> <br/>
-                <button onClick={() => gatherFilters(this.props.setFilter)}>Refresh</button>
+                <input
+                    type="text"
+                    id="limit"
+                    value={this.state.limit.toString()}
+                    onChange={this.handleLimit}
+                /> <br/>
+                <button onClick={this.handleSubmit}>Refresh</button>
             </div>
         );
     }
 }
 
-export interface TabPops {
+export interface TabProps {
     dataView: DataView;
     hidden: boolean;
     filters: ConseilFilter;
 }
 
-const Tab = (props: TabPops) => {
+const Tab = (props: TabProps) => {
     Conseil.getBlockHead('alphanet').then((val) => document.getElementById('poop').innerText = val);
     return(
         <div id="blocksPanel" hidden={props.hidden}>
