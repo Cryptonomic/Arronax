a0c0fa4bebf252f3815b0986eb82f5b937a6ba1a
Oleksandr Oleksiv <oleksiv1994@gmail.com>
2018-11-15 15:38:33 +0200

    allow configuration from external file

--- src/components/Arronax.tsx
+++ src/components/Arronax.tsx
@@ -3,6 +3,7 @@ import { TezosFilter } from 'conseiljs';
 import { Layout } from 'antd';
 import { FilterPanel } from './FilterPanel/FilterPanel';
 import { DataPanel } from './DataPanel';
+import config from '../config';
 /* tslint:disable no-import-side-effect */
 import '../App.css';
 
@@ -14,17 +15,49 @@ export interface ArronaxProps {
   setFilter(filters: TezosFilter, network: string): void;
 }
 
-export default (props: ArronaxProps): JSX.Element => (
-  <Layout style={{ height: '100%' }}>
-    {/* <Header>Arronax</Header> */}
-    <Sider>
-      <FilterPanel filters={props.filters} setFilter={props.setFilter} network={props.network} />
-    </Sider>
-    <Content style={{ padding: '50px' }}>
-      <DataPanel
-        filters={props.filters}
-        network={props.network}
-      />
-    </Content>
-  </Layout>
-);
+export interface ArronaxState {
+    loaded: boolean;
+}
+
+class Arronax extends React.PureComponent<ArronaxProps, ArronaxState> {
+    constructor(props: ArronaxProps) {
+        super(props);
+        this.state = {
+            loaded: false
+        };
+    }
+
+    componentDidMount() {
+        fetch('config.json')
+            .then(res => res.json())
+            .then(parsedRes => {
+                config.url = parsedRes.url;
+                config.key = parsedRes.key;
+                this.setState({loaded: true});
+            })
+            .catch(() => console.log('Error during config initialization.'))
+    }
+
+    render(): JSX.Element {
+        let arronaxView = <p>Fetching data....</p>;
+        if (this.state.loaded) {
+            arronaxView = (
+                <Layout style={{ height: '100%' }}>
+                    {/* <Header>Arronax</Header> */}
+                    <Sider>
+                        <FilterPanel filters={this.props.filters} setFilter={this.props.setFilter} network={this.props.network} />
+                    </Sider>
+                    <Content style={{ padding: '50px' }}>
+                        <DataPanel
+                            filters={this.props.filters}
+                            network={this.props.network}
+                        />
+                    </Content>
+                </Layout>
+            );
+        }
+        return arronaxView;
+    }
+}
+
+export default Arronax;
--- src/config.tsx
+++ src/config.tsx
@@ -3,14 +3,9 @@ type Config = {
   key: string;
 };
 
-const betanet: Config = {
-  url: 'http://conseil-prod.cryptonomic-infra.tech/tezos/',
-  key: 'hooman'
-};
-
-const zeronet: Config = {
+const defaultConfig: Config = {
   url: 'https://conseil-staging.cryptonomic-infra.tech/tezos/',
   key: 'hooman'
 };
 
-export default (process.env.NODE_ENV === 'betanet' ? betanet : zeronet);
+export default defaultConfig;
