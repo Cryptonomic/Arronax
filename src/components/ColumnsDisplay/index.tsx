3e214f3f3e2f9938f6eeba6f9a7337c0d4bba02a
Sean M <sean@arrowburn.co.nz>
2019-01-28 11:17:30 +1300

    column selection state working

--- src/components/ColumnsDisplay/index.tsx
+++ src/components/ColumnsDisplay/index.tsx
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
-import InputLabel from '@material-ui/core/InputLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import ListItemText from '@material-ui/core/ListItemText';
 import getDetailsColumns from 'src/utils/getDetailsColumns';
@@ -25,21 +24,29 @@ class ColumnDisplay extends React.Component {
   state = {
     selected: [],
   };
+
   handleChange = event => {
-    console.log(event.target.value);
+    const { selected } = this.state;
+    const checked = [...event.target.value];
+    const currentState = [...selected];
+    this.setState({
+      selected: [...checked],
+    });
   };
+
   render() {
-    // return <Container>Columns</Container>;
     const { selected } = this.state;
     return (
       <Container>
         <FormControl>
-          <InputLabel htmlFor="select-multiple-checkbox">Tag</InputLabel>
           <Select multiple value={selected} onChange={this.handleChange}>
+            {' '}
             {getDetailsColumns('blocks').map(name => (
               <MenuItem key={name.key} value={name.dataIndex}>
-                <Checkbox />
-                {/* checked={selected.indexOf(name) > -1} */}
+                <Checkbox
+                  disableRipple={true}
+                  checked={selected.indexOf(name.dataIndex) > -1}
+                />
                 <ListItemText primary={name.title} />
               </MenuItem>
             ))}
