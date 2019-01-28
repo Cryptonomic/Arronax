0f405206ca8443f589d4a524655f3084cdff58f9
Sean M <sean@arrowburn.co.nz>
2019-01-29 10:40:24 +1300

    add in redux dev tools

--- src/utils/getDetailsColumns.ts
+++ src/utils/getDetailsColumns.ts
@@ -92,3 +92,26 @@ const getDetailsColumns = (category: string) => {
 };
 
 export default getDetailsColumns;
+
+// case 'blocks':
+//       return [
+//         { title: 'Level', dataIndex: 'level', key: 'level' },
+//         { title: 'Block hash', dataIndex: 'hash', key: 'blockHash' },
+//         { title: 'Timestamp', dataIndex: 'timestamp', key: 'timestamp' },
+//       ];
+//     case 'operations':
+//       return [
+//         { title: 'Block hash', dataIndex: 'blockHash', key: 'blockHash' },
+//         { title: 'Kind', dataIndex: 'kind', key: 'kind' },
+//         { title: 'Source', dataIndex: 'source', key: 'source' },
+//         { title: 'Destination', dataIndex: 'destination', key: 'destination' },
+//         { title: 'Balance', dataIndex: 'balance', key: 'balance' },
+//         { title: 'Timestamp', dataIndex: 'timestamp', key: 'timestamp' },
+//       ];
+//     case 'accounts':
+//       return [
+//         { title: 'Account ID', dataIndex: 'accountId', key: 'accountId' },
+//         { title: 'Manager', dataIndex: 'manager', key: 'manager' },
+//         { title: 'Block level', dataIndex: 'blockLevel', key: 'blockLevel' },
+//         { title: 'Balance', dataIndex: 'balance', key: 'balance' },
+//       ];
