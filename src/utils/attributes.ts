75c416d7f50aac61a0c411daa40d8e3438021ee3
Sean M <sean@arrowburn.co.nz>
2019-03-27 09:24:30 +1300

    resolved merge conflicts

---
+++ src/utils/attributes.ts
@@ -0,0 +1,24 @@
+export const getLocalAttributes = () => {
+  const attributes = localStorage.getItem('attributes');
+  if (attributes) {
+    return JSON.parse(attributes);
+  }
+  return {
+    blocks: [],
+    operations: [],
+    accounts: [],
+  };
+};
+
+export const saveAttributes = (attributes, blockHead) => {
+  localStorage.setItem('attributes', JSON.stringify(attributes));
+  localStorage.setItem('blockHead', blockHead);
+};
+
+export const getBlockHeadFromLocal = () => {
+  const blockHead = localStorage.getItem('blockHead');
+  if (blockHead) {
+    return Number(blockHead);
+  }
+  return 0;
+};
