c29e0d23b6ee7405e1e9b1994341c6e95f388e3b
Crypto Mike <cryptomike@thinedge.local>
2019-05-10 14:15:53 -0400

    - more manual entity links

--- src/components/CustomTableRow/index.tsx
+++ src/components/CustomTableRow/index.tsx
@@ -94,7 +94,9 @@ interface Props {
 const formatValueForPrimary = (entity, name, shortValue, value, onClickPrimaryKey) => {
   if (PrimaryKeyList[entity].includes(name)) {
     return <LinkDiv onClick={() => onClickPrimaryKey(name, value)}>{shortValue}</LinkDiv>;
-  } else if (entity === 'accounts' && name === 'manager') {
+  } else if (entity === 'accounts' && name === 'manager') { // TODO: resolve via metadata
+    return <LinkDiv onClick={() => onClickPrimaryKey(name, value)}>{shortValue}</LinkDiv>;  
+  } else if (entity === 'blocks' && name === 'predecessor') { // TODO: resolve via metadata
     return <LinkDiv onClick={() => onClickPrimaryKey(name, value)}>{shortValue}</LinkDiv>;  
   }
   return shortValue;
