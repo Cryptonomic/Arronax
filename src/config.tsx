f9f16e9d888f59228c21ac76de5dd6178d44511b
anonymoussprocket <42718855+anonymoussprocket@users.noreply.github.com>
2019-06-05 12:01:44 -0400

    fix Travis build error

---
+++ src/config.tsx
@@ -0,0 +1,14 @@
+import { Config } from './types';
+
+const configs: Config[] = [
+  {
+    platform: 'platform',
+    network: 'network',
+    displayName: 'Not Configured',
+    url: 'conseil.server:port',
+    apiKey: 'apikey',
+    entities: []
+  }
+]
+
+export default configs;
