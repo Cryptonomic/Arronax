9dec027336c699284e6cc367e5d6efffed923c4d
anonymoussprocket <42718855+anonymoussprocket@users.noreply.github.com>
2019-06-09 09:34:29 -0400

    use displayNamePlural in tab headings (#215)

--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -298,7 +298,7 @@ class Arronax extends React.Component<Props, States> {
                   <TabWrapper
                     key={index}
                     value={entity.name}
-                    label={entity.displayName}
+                    label={entity.displayNamePlural}
                   />
                 ))}
               </TabsWrapper>
