c5b0eec6a6b5c3f8d88e0b421411344f22e2c022
developer0623 <developer0623@yahoo.com>
2019-08-05 15:51:41 -0400

    upgraded the babel to version 7

--- src/utils/getconfig.ts
+++ src/utils/getconfig.ts
@@ -16,7 +16,7 @@ export const saveConfigs = (configs: Config[]) => {
 export function getConfigs() {
     const localConfigs = getLocalConfigs();
     try {
-        const configs = require('../config');
+        const configs = require('../config').default;
         return [...configs, ...localConfigs];
     } catch (err) {
         return (localConfigs.length > 0) ? localConfigs : defaultConfig;
