fb40ee99a1d54ef592d30c72b319440e54047771
anonymoussprocket <anonymoussprocket@gmail.com>
2019-12-13 12:51:56 -0500

    - additional operation templates

--- src/containers/Entities/operation_template.tsx
+++ src/containers/Entities/operation_template.tsx
@@ -1,4 +1,4 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react';
 import { withTranslation, WithTranslation } from 'react-i18next';
 import Modal from '@material-ui/core/Modal';
 import IconButton from '@material-ui/core/IconButton';
@@ -175,6 +175,124 @@ class EntityModal extends React.Component<Props, States> {
                       <RowContainer>
                         <TitleTxt>{t('attributes.operations.operation_group_hash')}</TitleTxt>
                         <ContentTxt>{this.formatValue(processedValues, attributes, 'operation_group_hash')}</ContentTxt>
+                      </RowContainer>
+                    </Fragment>
+                  )}
+                  {opKind === 'endorsement' && (
+                    <Fragment>
+                      <RowContainer>
+                        <TitleTxt>{this.formatValue(processedValues, attributes, 'kind')}</TitleTxt>
+                        <ContentTxt>{this.formatValue(processedValues, attributes, 'branch')} {t('components.entityModal.at')} {this.formatValue(processedValues, attributes, 'level')}</ContentTxt>
+                      </RowContainer>
+
+                      <RowContainer>
+                        <TitleTxt>{t('attributes.operations.delegate')}</TitleTxt>
+                        <ContentTxt>{this.formatValue(processedValues, attributes, 'delegate')}</ContentTxt>
+                      </RowContainer>
+
+                      <RowContainer>
+                        <TitleTxt>{t('attributes.operations.slots')}</TitleTxt>
+                        <ContentTxt>{this.formatValue(processedValues, attributes, 'number_of_slots')}: {this.formatValue(processedValues, attributes, 'slots')}</ContentTxt>
+                      </RowContainer>
+
+                      <RowContainer>
+                        <TitleTxt>{t('attributes.operations.timestamp')}</TitleTxt>
+                        <ContentTxt>{this.formatValue(processedValues, attributes, 'timestamp')} {t('components.entityModal.in')} {this.formatValue(processedValues, attributes, 'block_hash')} {t('components.entityModal.at')} {this.formatValue(processedValues, attributes, 'block_level')} {t('components.entityModal.of')} {this.formatValue(processedValues, attributes, 'cycle')}
+                        </ContentTxt>
+                      </RowContainer>
+                    </Fragment>
+                  )}
+                  {opKind === 'delegation' && (
+                    <Fragment>
+                      <RowContainer>
+                        <TitleTxt>{this.formatValue(processedValues, attributes, 'kind')}</TitleTxt>
+                        {(processedValues.find(i => i.name === 'delegate') === undefined) && (
+                          <ContentTxt>{this.formatValue(processedValues, attributes, 'source')} {t('components.entityModal.to')} {t('components.entityModal.clear')}</ContentTxt>
+                        )}
+
+                        {(processedValues.find(i => i.name === 'delegate') !== undefined) && (
+                          <ContentTxt>{this.formatValue(processedValues, attributes, 'source')} {t('components.entityModal.to')} {this.formatValue(processedValues, attributes, 'delegate')}</ContentTxt>
+                        )}
+                      </RowContainer>
+
+                      <RowContainer>
+                        <TitleTxt>{t('attributes.operations.timestamp')}</TitleTxt>
+                        <ContentTxt>{this.formatValue(processedValues, attributes, 'timestamp')} {t('components.entityModal.in')} {this.formatValue(processedValues, attributes, 'block_hash')} {t('components.entityModal.at')} {this.formatValue(processedValues, attributes, 'block_level')} {t('components.entityModal.of')} {this.formatValue(processedValues, attributes, 'cycle')}
+                        </ContentTxt>
+                      </RowContainer>
+
+                      <RowContainer>
+                        <TitleTxt>{t('attributes.operations.status')}</TitleTxt>
+                        <ContentTxt>
+                          {this.formatValue(processedValues, attributes, 'status')} {t('components.entityModal.in')} {this.formatValue(processedValues, attributes, 'operation_group_hash')}
+                        </ContentTxt>
+                      </RowContainer>
+
+                      <RowContainer>
+                        <TitleTxt>{t('attributes.operations.consumed_gas')}</TitleTxt>
+                        <ContentTxt>
+                          {this.formatValue(processedValues, attributes, 'consumed_gas')} {t('components.entityModal.of')} {this.formatValue(processedValues, attributes, 'gas_limit')}
+                        </ContentTxt>
+                      </RowContainer>
+                    </Fragment>
+                  )}
+                  {opKind === 'origination' && (
+                    <Fragment>
+                      <RowContainer>
+                        <TitleTxt>{this.formatValue(processedValues, attributes, 'kind')}</TitleTxt>
+                        <ContentTxt>
+                          {this.formatValue(processedValues, attributes, 'originated_contracts')} {t('components.entityModal.by')} {this.formatValue(processedValues, attributes, 'source')}
+                        </ContentTxt>
+                      </RowContainer>
+
+                      <RowContainer>
+                        <TitleTxt>{t('attributes.operations.timestamp')}</TitleTxt>
+                        <ContentTxt>{this.formatValue(processedValues, attributes, 'timestamp')} {t('components.entityModal.in')} {this.formatValue(processedValues, attributes, 'block_hash')} {t('components.entityModal.at')} {this.formatValue(processedValues, attributes, 'block_level')} {t('components.entityModal.of')} {this.formatValue(processedValues, attributes, 'cycle')}
+                        </ContentTxt>
+                      </RowContainer>
+
+                      <RowContainer>
+                        <TitleTxt>{t('attributes.operations.status')}</TitleTxt>
+                        <ContentTxt>
+                          {this.formatValue(processedValues, attributes, 'status')} {t('components.entityModal.in')} {this.formatValue(processedValues, attributes, 'operation_group_hash')}
+                        </ContentTxt>
+                      </RowContainer>
+
+                      <RowContainer>
+                        <TitleTxt>{t('attributes.operations.consumed_gas')}</TitleTxt>
+                        <ContentTxt>
+                          {this.formatValue(processedValues, attributes, 'consumed_gas')} {t('components.entityModal.of')} {this.formatValue(processedValues, attributes, 'gas_limit')}
+                        </ContentTxt>
+                      </RowContainer>
+                    </Fragment>
+                  )}
+                  {opKind === 'reveal' && (
+                    <Fragment>
+                      <RowContainer>
+                        <TitleTxt>{this.formatValue(processedValues, attributes, 'kind')}</TitleTxt>
+                        <ContentTxt>
+                          {this.formatValue(processedValues, attributes, 'public_key')} {t('components.entityModal.by')} {this.formatValue(processedValues, attributes, 'source')}
+                        </ContentTxt>
+                      </RowContainer>
+
+                      <RowContainer>
+                        <TitleTxt>{t('attributes.operations.timestamp')}</TitleTxt>
+                        <ContentTxt>{this.formatValue(processedValues, attributes, 'timestamp')} &nbsp; {t('components.entityModal.in')} &nbsp; {this.formatValue(processedValues, attributes, 'block_hash')} {t('components.entityModal.at')} {this.formatValue(processedValues, attributes, 'block_level')} {t('components.entityModal.of')} {this.formatValue(processedValues, attributes, 'cycle')}
+                        </ContentTxt>
+                      </RowContainer>
+
+                      <RowContainer>
+                        <TitleTxt>{t('attributes.operations.status')}</TitleTxt>
+                        <ContentTxt>
+                          {this.formatValue(processedValues, attributes, 'status')} &nbsp; {t('components.entityModal.in')} {this.formatValue(processedValues, attributes, 'operation_group_hash')}
+                        </ContentTxt>
+                      </RowContainer>
+
+                      <RowContainer>
+                        <TitleTxt>{t('attributes.operations.consumed_gas')}</TitleTxt>
+                        <ContentTxt>
+                          {this.formatValue(processedValues, attributes, 'consumed_gas')} {t('components.entityModal.of')} {this.formatValue(processedValues, attributes, 'gas_limit')}
+                        </ContentTxt>
                       </RowContainer>
                     </Fragment>
                   )}
