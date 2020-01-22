08f5e8c08ce1493eb07631008ef7b8ce00464c99
Darius <dariusz.sobkowicz@gmail.com>
2020-01-22 10:53:35 +0100

    auto hide tooltip

--- src/components/Clipboard/Clipboard-container.tsx
+++ src/components/Clipboard/Clipboard-container.tsx
@@ -1,9 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import ClipboardView from './Clipboard-view';
 
 const ClipboardContainer = ({ value }) => {
   const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    if (open) {
+      setTimeout(() => {
+        setOpen(false);
+      }, 500);
+    }
+  }, [open])
   
   const onSuccess = () => setOpen(true);
   const onClose = () => setOpen(false);
