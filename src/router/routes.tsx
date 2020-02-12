14a58f49a7fca1cd5709cd76cb622972b0a5525d
Darius <dariusz.sobkowicz@gmail.com>
2020-02-12 17:30:01 +0100

    refactor: routes component

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -3,18 +3,22 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 
 import { ArronaxApp } from '../containers/App';
 
-export default () => (
-  <Switch>
-      <Route 
-        exact path={[
-          '/:platform/:network/:entity/:id', 
-          '/:platform/:network/:entity/query/:id', 
-          '/:platform/:network/:entity'
-        ]} 
-        component={ArronaxApp} />
-      <Route exact path="/">
-        <Redirect to="/tezos/mainnet/blocks" />
-      </Route>
-      <Redirect to="/" />
-    </Switch>
-);
+export const defaultPath = '/tezos/mainnet/blocks';
+
+export default () => {
+  const routes = [
+    '/:platform/:network/:entity/:id', 
+    '/:platform/:network/:entity/query/:id', 
+    '/:platform/:network/:entity'
+  ];
+
+  return (
+    <Switch>
+        <Route exact path={routes} component={ArronaxApp} />
+        <Route exact path="/">
+          <Redirect to={defaultPath} />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+  );
+}
