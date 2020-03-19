f1d34ebf38d80d5c5a66f6932c447f22fda04849
Darius <dariusz.sobkowicz@gmail.com>
2020-03-19 10:09:44 +0100

    feat: separate share and export

--- src/components/Toolbar/index.tsx
+++ src/components/Toolbar/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import Tooltip from '@material-ui/core/Tooltip';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
+import Clipboard from '../Clipboard';
+
+
 import { ToolType } from '../../types';
 
 import {
@@ -11,10 +12,12 @@ import {
   ColumnsTool,
   AggTool,
   ShareTool,
+  ExportTool,
   FilterIcon,
   ExportIcon,
   ColumnIcon,
-  BadgeWrapper
+  BadgeWrapper,
+  TooltipWrapper
 } from './styles';
 
 import { ToolbarProps } from './types';
@@ -22,11 +25,10 @@ import { ToolbarProps } from './types';
 const Toolbar: React.FC<ToolbarProps> = props => {
   const { isCollapsed, selectedTool, filterCount, aggCount, columnsCount, onChangeTool, onExportCsv, onShareReport } = props;
   const [open, setOpen] = React.useState(false);
-  const [anchorEl, setAnchorEl] = React.useState(null);
   const { t } = useTranslation();
-  function shareReport() {
+  
+  const shareReport = () => {
     setOpen(true);
-    setAnchorEl(null);
     setTimeout(() => {
       onShareReport();
     });
@@ -35,61 +37,55 @@ const Toolbar: React.FC<ToolbarProps> = props => {
     }, 2000);
   }
 
-  function openShareMenu(event: any) {
-    setAnchorEl(event.currentTarget);
-  }
-  function closeShareMenu() {
-    setAnchorEl(null);
-  }
-  function exportCsv() {
-    setAnchorEl(null);
-    onExportCsv();
-  }
+  const onClose = () => setOpen(false);
+
   return (
     <Container>
-      <FilterTool
-        isactive={isCollapsed && selectedTool === ToolType.FILTER}
-        onClick={() => onChangeTool(ToolType.FILTER)}
-      >
-        <FilterIcon size="20px" color="#4a4a4a" iconName="icon-filter" />
-        <BadgeWrapper color="secondary" badgeContent={filterCount}>
-          {t('components.toolBar.filter')}
-        </BadgeWrapper>
-      </FilterTool>
-      <ColumnsTool
-        isactive={isCollapsed && selectedTool === ToolType.COLUMN}
-        onClick={() => onChangeTool(ToolType.COLUMN)}
-      >
-        <ColumnIcon size="20px" color="#4a4a4a" iconName="icon-columns" />
-        <BadgeWrapper color="secondary" badgeContent={columnsCount}>
-          {t('components.toolBar.columns')}
-        </BadgeWrapper>
-      </ColumnsTool>
-      <AggTool
-        isactive={isCollapsed && selectedTool === ToolType.AGGREGATION}
-        onClick={() => onChangeTool(ToolType.AGGREGATION)}
-      >
-        <ColumnIcon size="20px" color="#4a4a4a" iconName="icon-aggregate" />
-        <BadgeWrapper color="secondary" badgeContent={aggCount}>
-          {t('components.toolBar.aggregation')}
-        </BadgeWrapper>
-      </AggTool>
-      <ShareTool aria-controls="share-menu" aria-haspopup="true" onClick={openShareMenu}>
-        <ExportIcon size="20px" color="#4a4a4a" iconName="icon-export" />
-        {t('components.toolBar.share')}
-      </ShareTool>
-      <Menu
-        id="share-menu"
-        anchorEl={anchorEl}
-        keepMounted
-        open={Boolean(anchorEl)}
-        onClose={closeShareMenu}
-      >
-        <MenuItem onClick={exportCsv}>{t('components.toolBar.export_csv')}</MenuItem>
-        <Tooltip title="Copied!" placement="right-start" open={open}>
-          <MenuItem onClick={shareReport}>{t('components.toolBar.share_report')}</MenuItem>
-        </Tooltip>
-      </Menu>
+        <FilterTool
+          isactive={isCollapsed && selectedTool === ToolType.FILTER}
+          onClick={() => onChangeTool(ToolType.FILTER)}
+        >
+          <FilterIcon size="20px" color="#4a4a4a" iconName="icon-filter" />
+          <BadgeWrapper color="secondary" badgeContent={filterCount}>
+            {t('components.toolBar.filter')}
+          </BadgeWrapper>
+        </FilterTool>
+        <ColumnsTool
+          isactive={isCollapsed && selectedTool === ToolType.COLUMN}
+          onClick={() => onChangeTool(ToolType.COLUMN)}
+        >
+          <ColumnIcon size="20px" color="#4a4a4a" iconName="icon-columns" />
+          <BadgeWrapper color="secondary" badgeContent={columnsCount}>
+            {t('components.toolBar.columns')}
+          </BadgeWrapper>
+        </ColumnsTool>
+        <AggTool
+          isactive={isCollapsed && selectedTool === ToolType.AGGREGATION}
+          onClick={() => onChangeTool(ToolType.AGGREGATION)}
+        >
+          <ColumnIcon size="20px" color="#4a4a4a" iconName="icon-aggregate" />
+          <BadgeWrapper color="secondary" badgeContent={aggCount}>
+            {t('components.toolBar.aggregation')}
+          </BadgeWrapper>
+        </AggTool>
+      <ExportTool aria-controls="share-menu" aria-haspopup="true" onClick={onExportCsv}>
+        <ExportIcon size="20px" color="#4a4a4a" iconName="icon-download" />
+        {t('components.toolBar.export')}
+      </ExportTool>
+      <TooltipWrapper PopperProps={{
+            disablePortal: true
+          }}
+          onClose={onClose}
+          open={open}
+          disableFocusListener
+          disableHoverListener
+          disableTouchListener
+          title="Copied!">
+        <ShareTool aria-controls="share-menu" aria-haspopup="true" onClick={shareReport}>
+          <ExportIcon size="20px" color="#4a4a4a" iconName="icon-share" />
+          {t('components.toolBar.share')}
+        </ShareTool>
+      </TooltipWrapper>
     </Container>
   );
 };
