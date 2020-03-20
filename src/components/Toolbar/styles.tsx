5f2af198d5df84b9e10370f730ca49aef5650f2f
Darius <dariusz.sobkowicz@gmail.com>
2020-03-20 14:42:05 +0100

    feat: removed toolbar highlight bar

--- src/components/Toolbar/styles.tsx
+++ src/components/Toolbar/styles.tsx
@@ -30,15 +30,6 @@ export const ToolItem = styled.div`
       color: rgb(86, 194, 217);
     }
   }
-  &:after {
-    content: '';
-    width: 100%;
-    height: 6px;
-    background: transparent;
-    position: absolute;
-    bottom: 0;
-    left: 0;
-  }
 `;
 
 export const FilterTool = styled(ToolItem)<{ isactive: boolean }>`
